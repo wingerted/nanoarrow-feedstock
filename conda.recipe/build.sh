c709693dcda0e41a0f6dadd3bbea3b10f3e816ca
wingerted <wingerted@gmail.com>
2025-02-17 00:51:04 +0800

    fix: use BUILD_PREFIX for compiler path

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,16 +4,14 @@ set -x
 
 
 # 显式指定使用 conda 环境中的 clang 和 clang++（此步可作为保险，同时 PATH 也已修改）
-export CC="$PREFIX/bin/clang"
-export CXX="$PREFIX/bin/clang++"
+export CC="$BUILD_PREFIX/bin/clang"
+export CXX="$BUILD_PREFIX/bin/clang++"
 
 mkdir -p build && cd build
 
 cmake ${CMAKE_ARGS} -GNinja \
     -DCMAKE_PREFIX_PATH="$PREFIX" \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DCMAKE_C_COMPILER="$CC" \
-    -DCMAKE_CXX_COMPILER="$CXX" \
     "$SRC_DIR"
 
 ninja
